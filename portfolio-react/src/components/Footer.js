d466164ccd969d85fae386d9fa43187dc9cfcf3e
Gursewak <samambarsarwala08@gmail.com>
2026-04-09 20:35:28 -0700

    Update Footer.js

--- portfolio-react/src/components/Footer.js
+++ portfolio-react/src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-dark text-light text-center py-3">
-      <p>© 2026 My Portfolio</p>
+      <p>© 2026 GuriDev Portfolio</p>
     </footer>
   );
 }
