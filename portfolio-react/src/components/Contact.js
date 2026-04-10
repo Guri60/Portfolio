8254f757002d8390d2a47bfe7b2ca71d791b2eb6
Gursewak <samambarsarwala08@gmail.com>
2026-04-09 20:34:54 -0700

    Update Contact.js

--- portfolio-react/src/components/Contact.js
+++ portfolio-react/src/components/Contact.js
@@ -3,8 +3,11 @@ function Contact() {
     <section id="contact" className="bg-light text-center py-5">
       <div className="container">
         <h2>Contact</h2>
-        <p>Email: your@email.com</p>
-        <button className="btn btn-dark">Get In Touch</button>
+        <p>Email: gslcreators.08@gmail.com</p>
+
+        <a href="mailto:gslcreators.08@email.com" className="btn btn-dark">
+          Get In Touch
+        </a>
       </div>
     </section>
   );
