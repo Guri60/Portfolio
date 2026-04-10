599b80bb605b63b21e60f2942879775431e2c46b
Gursewak <samambarsarwala08@gmail.com>
2026-04-09 20:36:58 -0700

    Update Navbar.js

--- portfolio-react/src/components/Navbar.js
+++ portfolio-react/src/components/Navbar.js
@@ -2,11 +2,16 @@ function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
       <div className="container">
-        <a className="navbar-brand" href="#">MyPortfolio</a>
-        <div>
-          <a href="#projects" className="nav-link d-inline text-light">Projects</a>
-          <a href="#contact" className="nav-link d-inline text-light">Contact</a>
-        </div>
+        <a className="navbar-brand fw-bold" href="#home">
+          Guri<span style={{ color: "#0d6efd" }}>Dev</span>
+        </a>
+        
+      <div className="d-flex gap-4">
+  <a href="#about" className="nav-link text-light">About</a>
+  <a href="#skills" className="nav-link text-light">Skills</a>
+  <a href="#projects" className="nav-link text-light">Projects</a>
+  <a href="#contact" className="nav-link text-light">Contact</a>
+</div>
       </div>
     </nav>
   );
